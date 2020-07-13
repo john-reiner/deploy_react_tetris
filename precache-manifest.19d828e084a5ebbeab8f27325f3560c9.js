31d8458df24ee7c77cc30899d1ea0169cdca29ff
jareiner90 <johnreiner1990@gmail.com>
2020-07-13 11:15:48 -0400

    Updates

--- precache-manifest.19d828e084a5ebbeab8f27325f3560c9.js
+++ precache-manifest.19d828e084a5ebbeab8f27325f3560c9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "52d8fb40959ac304479990473619a659",
+    "revision": "9fb037402784fc173139a555f4bd29a6",
     "url": "/deploy_react_tetris/index.html"
   },
   {
-    "revision": "c744c14f11383f5be56d",
+    "revision": "ad7fa23f1aedae228e7d",
     "url": "/deploy_react_tetris/static/css/main.64c8d670.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/deploy_react_tetris/static/js/2.781ed2fe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c744c14f11383f5be56d",
-    "url": "/deploy_react_tetris/static/js/main.eeb736e0.chunk.js"
+    "revision": "ad7fa23f1aedae228e7d",
+    "url": "/deploy_react_tetris/static/js/main.6116e806.chunk.js"
   },
   {
     "revision": "15ccef9c3741954eb54e",
