4eb5d93fd1f2a3c2b9dc8fe0b4b4bb220d4dbeb4
jareiner90 <johnreiner1990@gmail.com>
2020-07-13 10:47:38 -0400

    Updates

---
+++ precache-manifest.a772c9c942065cde795bf5f73c5ca780.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9af09ea979f836ef0892ab97f454ae",
+    "url": "/jareiner90/deploy_react_tetris.git/index.html"
+  },
+  {
+    "revision": "2f5bfce88655172b796c",
+    "url": "/jareiner90/deploy_react_tetris.git/static/css/main.254a9706.chunk.css"
+  },
+  {
+    "revision": "fea825171d11a548760c",
+    "url": "/jareiner90/deploy_react_tetris.git/static/js/2.7862061d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jareiner90/deploy_react_tetris.git/static/js/2.7862061d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5bfce88655172b796c",
+    "url": "/jareiner90/deploy_react_tetris.git/static/js/main.47512e25.chunk.js"
+  },
+  {
+    "revision": "b4180f2ea450e2d44c2a",
+    "url": "/jareiner90/deploy_react_tetris.git/static/js/runtime-main.4f21b161.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/jareiner90/deploy_react_tetris.git/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "c408b72519bfd8bcb12af061bdebbac9",
+    "url": "/jareiner90/deploy_react_tetris.git/static/media/background.c408b725.jpg"
+  }
+]);
