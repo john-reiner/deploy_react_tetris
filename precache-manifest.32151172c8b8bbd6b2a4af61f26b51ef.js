97350b18d46a11d5ab447e6b90d9c18f7d60218c
john-reiner <johnreiner1990@gmail.com>
2020-08-13 11:23:06 -0400

    Updates

---
+++ precache-manifest.32151172c8b8bbd6b2a4af61f26b51ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dbfc5fe8959edabb122e8db07d1d78",
+    "url": "/react_tetris/index.html"
+  },
+  {
+    "revision": "da388a4d5e781fbf093f",
+    "url": "/react_tetris/static/css/main.b5c363df.chunk.css"
+  },
+  {
+    "revision": "e05045a2e9aa62dee272",
+    "url": "/react_tetris/static/js/2.848434fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react_tetris/static/js/2.848434fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da388a4d5e781fbf093f",
+    "url": "/react_tetris/static/js/main.7e5c44b7.chunk.js"
+  },
+  {
+    "revision": "99a6d3f542eb76fd23c2",
+    "url": "/react_tetris/static/js/runtime-main.0d864a12.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react_tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "c408b72519bfd8bcb12af061bdebbac9",
+    "url": "/react_tetris/static/media/background.c408b725.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy_react_tetris/precache-manifest.b3908a547e9c1b838bf222d8fc4734f8.js"
+  "/react_tetris/precache-manifest.32151172c8b8bbd6b2a4af61f26b51ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy_react_tetris/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_tetris/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
